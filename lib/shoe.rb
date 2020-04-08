6f2b8ab4219dc1bb5a0952f25cb261408788ffb2
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-08 02:14:39 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,12 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  BRANDS =[]
 
-  def initialize(brand)
-    @brand = brand
-  end
+ def initialize(brand)
+   @brand = brand 
+   BRANDS << brand 
+ end 
 
   def cobble
     self.condition = "new"
